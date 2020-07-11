7f50b1b80d94485d9afe8bde09f86f68b332e90c
Etash Guha <etashthebomb@gmail.com>
2020-07-10 22:56:42 -0400

    training with shallow neural net end

--- src/dagger.py
+++ src/dagger.py
@@ -184,7 +184,7 @@ class RankDagger(Dagger):
                     os.remove(self.save_path)
                 torch.save(self.policy.state_dict(), self.save_path)
     def test(self, problems):
-        super().test(problems, self.nodesel)
+        return super().test(problems, self.nodesel)
 
 class TreeDagger(Dagger):
     def __init__(self, selector, problem_dir, device, num_train=None, num_epoch = 1, batch_size=5, save_path=None, num_repeat=1):
@@ -289,4 +289,4 @@ class TreeDagger(Dagger):
 
 
     def test(self, problems):
-        super().test(problems, self.nodesel)
+        return super().test(problems, self.nodesel)
