ecb40de4f5fce4f114442665782d092786a2648b
Etash Guha <etashthebomb@gmail.com>
2020-10-13 16:01:03 -0400

    node calculation'

--- src/test/test_nodesel.py
+++ src/test/test_nodesel.py
@@ -1,5 +1,7 @@
 import torch.sparse
 import faulthandler
+import sys
+import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from TreeLSTM import TreeLSTMCell, TreeLSTM, LinLib, ShallowLib
